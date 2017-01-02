bcdd15da7d4a6eb3c5757c67c4a5ed0a9dd5bf84
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2017-01-02 21:10:42 +0100

    Added GDG Port Harcourt Social Links

--- app/services/configService.js
+++ app/services/configService.js
@@ -2,15 +2,15 @@ angular.module('gdgXBoomerang')
 .factory('Config', function () {
     return {
         // TODO Modify these to configure your app
-        'name'          : 'GDG Space Coast',
-        'id'            : '103959793061819610212',
-        'googleApi'     : '<insert your API key here>',
+        'name'          : 'GDG Port Harcourt',
+        'id'            : '100461466728650662561',
+        'googleApi'     : 'AIzaSyC6hw2FdbmEkUOenU5Tc_6N9wCCAcCazlk',
         'pwaId'         : '5915725140705884785', // Picasa Web Album id, must belong to Google+ id above
-        'domain'        : 'http://www.gdgspacecoast.org',
-        'twitter'       : 'gdgspacecoast',
-        'facebook'      : 'gdgspacecoast',
-        'youtube'       : 'UCkiYHK3IZMk5XsYZ626b9Rw',
-        'meetup'        : 'gdgspacecoast',
+        'domain'        : 'http://www.phc.gdg.ng',
+        'twitter'       : 'GDGPHC',
+        'facebook'      : 'gdgportharcourt',
+        'youtube'       : 'UCX83RpHPrQwTBJ46xCtmIjw',
+        'meetup'        : '#',
         // Change to 'EEEE, MMMM d, y - H:mm' for 24 hour time format.
         'dateFormat'    : 'EEEE, MMMM d, y - h:mm a',
         'cover' : {
@@ -18,7 +18,7 @@ angular.module('gdgXBoomerang')
             subtitle: 'Directory of developer events organized by tags and displayed on a global map.',
             button: {
                 text: 'Find local events',
-                url: 'http://gdg.events/'
+                url: 'http://developers.google.com/events'
             }
         },
         'activities': {
